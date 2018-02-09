ca7662d1e75a0f99d549748354c332f192d0ab92
Marin Cabac <marin.cabac@gmail.com>
2018-02-09 18:57:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-def reverse_each_word(sentence)
-  reversed = (sentence.split.collect do |word|
+def reverse_each_word(s)
+  rev = (s.split.collect {|word|
+
     
-    word.reverse
-  end).join(' ')
+    word.reverse}.join(' ')
 end
